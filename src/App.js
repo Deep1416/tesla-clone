4250cadd34654e4989384f7306dfdfff8f0dede5
Deependra <deependra186037@gamil.com>
2024-04-01 00:57:13 +0530

    add data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import Responsive from './Components/Responsive';
 
 function App() {
   return (
     <>
     <Header/>
-    <Home/>
+    {/* <Home/> */}
+    <Responsive/>
     </>
   );
 }
